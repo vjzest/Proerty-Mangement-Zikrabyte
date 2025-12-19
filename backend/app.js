ef79cc7e53a7c89f043cb2274ba334b525ebe129
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2025-12-19 16:56:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,44 +5,35 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const propertyRoutes = require("./routes/propertyRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
-const inquiryRouter = require('./routes/inquiryRoutes');
-
+const inquiryRouter = require("./routes/inquiryRoutes");
 
 const app = express();
-
-// CORS
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://proerty-mangement-zikrabyte-u8eu.vercel.app"]
+    origin: [
+      "http://localhost:3000",
+      "https://proerty-mangement-zikrabyte-u8eu.vercel.app",
+    ],
     credentials: true,
   })
 );
-
-// Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/properties", propertyRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
-app.use('/api/v1/inquiries', inquiryRouter);
-
-
-// Health
+app.use("/api/v1/inquiries", inquiryRouter);
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "success",
     message: "Server is running",
   });
 });
-
-// 404
 app.use((req, res) => {
-  res.status(200).json({
-    status: "Success",
-    message: `Server Runing Successfully ${req.originalUrl}`,
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl}`,
   });
 });
 
